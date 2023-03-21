a4504be8817b6b8cc287fdc2b1f013fb9e041aff
Lonedarth10 <darthselma10@gmail.com>
2023-03-20 17:14:41 -0700

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,8 +23,8 @@ typedef struct dog my_dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+my_dog *new_dog(char *name, float age, char *owner);
+void free_dog(my_dog *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
